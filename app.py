709144c405ed56915304d2d01bdb5996347a379d
Sandeep <shirolesandeepu@rediffmail.com>
2022-06-12 22:41:20 +0100

    app msg new

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "starting ml project"
+    return "starting ml project from ci cd completed"
 
 
 if __name__=="__main__":
